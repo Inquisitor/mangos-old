398a084ff0a777eaf9b626a645022bc1a469d4ef
balrok <der-coole-carl@gmx.net>
2009-01-22 10:26:38 +0100

    better errorreport if setbg on bgmap fails
    please look at your mangoslogs if it crashes

--- src/game/MapInstanced.cpp
+++ src/game/MapInstanced.cpp
@@ -152,7 +152,16 @@ Map* MapInstanced::GetInstance(const WorldObject* obj)
                 {
                     map = CreateBattleGround(NewInstanceId);
                     ((BattleGroundMap*)map)->SetBG(player->GetBattleGround());
-                    assert(((BattleGroundMap*)map)->GetBG());
+                }
+                if(!((BattleGroundMap*)map)->GetBG())
+                {
+                    sLog.outError("The bg-class couldn't be assigned (very early) to the battlegroundmap, it's possible, that some db-spawned creatures are now not handled right this is related to battleground alterac valley (av) - please post bugreport, and add information how this bg was created (if you don't have information, report it also) Player: %s (%u) in map:%u requested map:%u", player->GetName(), player->GetGUIDLow(), player->GetMapId(), GetId());
+                    if(player->GetBattleGround())
+                    {
+                        sLog.outError("somehow the battleground was found, but please report also");
+                        ((BattleGroundMap*)map)->SetBG(player->GetBattleGround());
+                    }
+                    assert(false);
                 }
                 return map;
             }
