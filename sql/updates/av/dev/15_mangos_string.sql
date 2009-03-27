5639f88ca41bc476766beeae78afc7b4c5ab9e39
balrok <der-coole-carl@gmx.net>
2009-03-27 22:34:31 +0100

    moved language-ids from custom to battleground

--- sql/updates/av/dev/15_mangos_string.sql
+++ sql/updates/av/dev/15_mangos_string.sql
@@ -1,81 +1,81 @@
-delete from mangos_string where entry>10746 and entry<10781;
+delete from mangos_string where entry>756 and entry<791;
 INSERT INTO mangos_string VALUES
-(10747,'Alliance',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10748,'Horde',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10749,'%s was destroyed by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10750,'The %s is under attack! If left unchecked, the %s will destroy it!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10751,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10752,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10753,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10754,'The %s is under attack! If left unchecked, the %s will capture it!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10755,'The %s has taken the %s! Its supplies will now be used for reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10756,'Irondeep Mine',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10757,'Coldtooth Mine',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10758,'Stormpike Aid Station',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10759,'Dun Baldar South Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10760,'Dun Baldar North Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10761,'Stormpike Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10762,'Icewing Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10763,'Stonehearth Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10764,'Stonehearth Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10765,'Snowfall Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10766,'Iceblood Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10767,'Iceblood Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10768,'Tower Point',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10769,'Frostwolf Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10770,'East Frostwolf Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10771,'West Frostwolf Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10772,'Frostwolf Relief Hut',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10773,'2 minute until the battle for Alterac Valley begins.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10774,'1 minute until the battle for Alterac Valley begins.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10775,'30 seconds until the battle for Alterac Valley begins. Prepare yourselves!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10776,'The battle for Alterac Valley has begun!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10777,'The Alliance Team is running out of reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10778,'The Horde Team is running out of reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10779,'The Frostwolf General is Dead!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(10780,'The Stormpike General is Dead!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)
+(757,'Alliance',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(758,'Horde',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(759,'%s was destroyed by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(760,'The %s is under attack! If left unchecked, the %s will destroy it!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(761,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(762,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(763,'The %s was taken by the %s!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(764,'The %s is under attack! If left unchecked, the %s will capture it!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(765,'The %s has taken the %s! Its supplies will now be used for reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(766,'Irondeep Mine',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(767,'Coldtooth Mine',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(768,'Stormpike Aid Station',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(769,'Dun Baldar South Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(770,'Dun Baldar North Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(771,'Stormpike Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(772,'Icewing Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(773,'Stonehearth Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(774,'Stonehearth Bunker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(775,'Snowfall Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(776,'Iceblood Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(777,'Iceblood Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(778,'Tower Point',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(779,'Frostwolf Graveyard',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(780,'East Frostwolf Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(781,'West Frostwolf Tower',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(782,'Frostwolf Relief Hut',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(783,'2 minute until the battle for Alterac Valley begins.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(784,'1 minute until the battle for Alterac Valley begins.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(785,'30 seconds until the battle for Alterac Valley begins. Prepare yourselves!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(786,'The battle for Alterac Valley has begun!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(787,'The Alliance Team is running out of reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(788,'The Horde Team is running out of reinforcements!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(789,'The Frostwolf General is Dead!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(790,'The Stormpike General is Dead!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)
 ;
 
-UPDATE mangos_string SET content_loc3='Allianz' WHERE entry=10747;
-UPDATE mangos_string SET content_loc3='Horde' WHERE entry=10748;
-UPDATE mangos_string SET content_loc3='%s wurde von der %s zerstört!' WHERE entry=10749;
-UPDATE mangos_string SET content_loc3='%s wird angegriffen! Wenn er unbewacht bleibt, wird die %s ihn zerstören!' WHERE entry=10750;
+UPDATE mangos_string SET content_loc3='Allianz' WHERE entry=757;
+UPDATE mangos_string SET content_loc3='Horde' WHERE entry=758;
+UPDATE mangos_string SET content_loc3='%s wurde von der %s zerstört!' WHERE entry=759;
+UPDATE mangos_string SET content_loc3='%s wird angegriffen! Wenn er unbewacht bleibt, wird die %s ihn zerstören!' WHERE entry=760;
 -- Wenn _sie_ unbewacht bleibt wird allianz _sie_ TODO: don't store towers and gravenames, store complete strings with the names inside it, else i don't know how to implement this feature
 
-UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=10751;
-UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=10752;
-UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=10753;
+UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=761;
+UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=762;
+UPDATE mangos_string SET content_loc3='%s wurde von der %s erobert!' WHERE entry=763;
 
-UPDATE mangos_string SET content_loc3='%s wird angegriffen! Wenn er unbewacht bleibt, wird die %s ihn erobern!' WHERE entry=10754;
+UPDATE mangos_string SET content_loc3='%s wird angegriffen! Wenn er unbewacht bleibt, wird die %s ihn erobern!' WHERE entry=764;
 -- Wenn _sie_ unbewacht bleibt ... TODO...
 
-UPDATE mangos_string SET content_loc3='Die %s hat die %s eingenommen! Die dort befindlichen Vorräte werden zur Verstärkung eingesetzt!' WHERE entry=10755;
+UPDATE mangos_string SET content_loc3='Die %s hat die %s eingenommen! Die dort befindlichen Vorräte werden zur Verstärkung eingesetzt!' WHERE entry=765;
 
-UPDATE mangos_string SET content_loc3='Eisenschachtmine' WHERE entry=10756;
-UPDATE mangos_string SET content_loc3='Eisbeißermine' WHERE entry=10757;
-UPDATE mangos_string SET content_loc3='Das Lazarett der Sturmlanzen' WHERE entry=10758;
-UPDATE mangos_string SET content_loc3='Der Südbunker von Dun Baldar' WHERE entry=10759;
-UPDATE mangos_string SET content_loc3='Der Nordbunker von Dun Baldar' WHERE entry=10760;
-UPDATE mangos_string SET content_loc3='Der Friedhof der Sturmlanzen' WHERE entry=10761;
-UPDATE mangos_string SET content_loc3='Der Eisschwingenbunker' WHERE entry=10762;
-UPDATE mangos_string SET content_loc3='Der Steinbruchfriedhof' WHERE entry=10763;
-UPDATE mangos_string SET content_loc3='Der Steinbruchbunker' WHERE entry=10764;
-UPDATE mangos_string SET content_loc3='Der Schneewehenfriedhof' WHERE entry=10765;
-UPDATE mangos_string SET content_loc3='Der Eisblutturm' WHERE entry=10766;
-UPDATE mangos_string SET content_loc3='Der Eisblutfriedhof' WHERE entry=10767;
-UPDATE mangos_string SET content_loc3='Die Turmstellung' WHERE entry=10768;
-UPDATE mangos_string SET content_loc3='Der Friedhof der Frostwölfe' WHERE entry=10769;
-UPDATE mangos_string SET content_loc3='Der östliche Frostwolfturm' WHERE entry=10770;
-UPDATE mangos_string SET content_loc3='Der westliche Frostwolfturm' WHERE entry=10771;
-UPDATE mangos_string SET content_loc3='Die Heilerhütte der Frostwölfe' WHERE entry=10772;
-UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 2 Minuten.' WHERE entry=10773;
-UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 1 Minute.' WHERE entry=10774;
-UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 30 Sekunden.' WHERE entry=10775;
-UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal hat begonnen!' WHERE entry=10776;
-UPDATE mangos_string SET content_loc3='Die Allianz hat nurnoch wenige Verstärkungspunkte' WHERE entry=10777; -- TODO find right msg
-UPDATE mangos_string SET content_loc3='Die Horde hat nurnoch wenige Verstärkungspunkte' WHERE entry=10778; -- TODO same here
-UPDATE mangos_string SET content_loc3='Der General der Allianz ist tot!' WHERE entry=10779;
-UPDATE mangos_string SET content_loc3='Der General der Horde ist tot!' WHERE entry=10780;
+UPDATE mangos_string SET content_loc3='Eisenschachtmine' WHERE entry=766;
+UPDATE mangos_string SET content_loc3='Eisbeißermine' WHERE entry=767;
+UPDATE mangos_string SET content_loc3='Das Lazarett der Sturmlanzen' WHERE entry=768;
+UPDATE mangos_string SET content_loc3='Der Südbunker von Dun Baldar' WHERE entry=769;
+UPDATE mangos_string SET content_loc3='Der Nordbunker von Dun Baldar' WHERE entry=770;
+UPDATE mangos_string SET content_loc3='Der Friedhof der Sturmlanzen' WHERE entry=771;
+UPDATE mangos_string SET content_loc3='Der Eisschwingenbunker' WHERE entry=772;
+UPDATE mangos_string SET content_loc3='Der Steinbruchfriedhof' WHERE entry=773;
+UPDATE mangos_string SET content_loc3='Der Steinbruchbunker' WHERE entry=774;
+UPDATE mangos_string SET content_loc3='Der Schneewehenfriedhof' WHERE entry=775;
+UPDATE mangos_string SET content_loc3='Der Eisblutturm' WHERE entry=776;
+UPDATE mangos_string SET content_loc3='Der Eisblutfriedhof' WHERE entry=777;
+UPDATE mangos_string SET content_loc3='Die Turmstellung' WHERE entry=778;
+UPDATE mangos_string SET content_loc3='Der Friedhof der Frostwölfe' WHERE entry=779;
+UPDATE mangos_string SET content_loc3='Der östliche Frostwolfturm' WHERE entry=780;
+UPDATE mangos_string SET content_loc3='Der westliche Frostwolfturm' WHERE entry=781;
+UPDATE mangos_string SET content_loc3='Die Heilerhütte der Frostwölfe' WHERE entry=782;
+UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 2 Minuten.' WHERE entry=783;
+UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 1 Minute.' WHERE entry=784;
+UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal beginnt in 30 Sekunden.' WHERE entry=785;
+UPDATE mangos_string SET content_loc3='Der Kampf um das Alteractal hat begonnen!' WHERE entry=786;
+UPDATE mangos_string SET content_loc3='Die Allianz hat nurnoch wenige Verstärkungspunkte' WHERE entry=787; -- TODO find right msg
+UPDATE mangos_string SET content_loc3='Die Horde hat nurnoch wenige Verstärkungspunkte' WHERE entry=788; -- TODO same here
+UPDATE mangos_string SET content_loc3='Der General der Allianz ist tot!' WHERE entry=789;
+UPDATE mangos_string SET content_loc3='Der General der Horde ist tot!' WHERE entry=790;
 
 
 -- CHAT_MSG_BG_SYSTEM_ALLIANCE', 'txt': '$N hat den Schneewehenfriedhof besetzt! Wenn niemand eingreift, erlangt die Allianz die Kontrolle \xc3\xbcber ihn
