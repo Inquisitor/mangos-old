6190b78640107337e13284ac2704f463fef57f13
Feanor <maly32167@gmail.com>
2009-04-16 00:06:44 +0200

    Quest Incrementing thru Database Script System

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -3839,6 +3839,26 @@ void ObjectMgr::LoadScripts(ScriptMapMap& scripts, char const* tablename)
                 }
                 break;
             }
+			case SCRIPT_COMMAND_ADD_QUEST_COUNT:
+			{
+				Quest const* quest = GetQuestTemplate(tmp.datalong);
+				if( tmp.datalong2 > 4 )
+				{
+					sLog.outErrorDb("Table `%s` has x for requirement [] (x: %u) in SCRIPT_COMMAND_ADD_QUEST_COUNT in `datalong2` for script id %u",tablename,tmp.datalong2,tmp.id);
+					continue;
+				}
+				if( tmp.dataint < 1 )
+				{
+					sLog.outErrorDb("Table `%s` has increment value (Value: %u) in SCRIPT_COMMAND_ADD_QUEST_COUNT in `dataint` for script id %u",tablename,tmp.dataint,tmp.id);
+					continue;
+				}
+				if(!quest)
+				{
+					sLog.outErrorDb("Table `%s` has invalid quest (ID: %u) in SCRIPT_COMMAND_ADD_QUEST_COUNT in `datalong` for script id %u",tablename,tmp.datalong,tmp.id);
+					continue;
+				}
+				break;
+			} 
         }
 
         if (scripts.find(tmp.id) == scripts.end())
@@ -3916,7 +3936,7 @@ void ObjectMgr::LoadSpellScripts()
             if( !spellInfo->Effect[i] )
                 continue;
 
-            if( spellInfo->Effect[i] == SPELL_EFFECT_SCRIPT_EFFECT )
+            if( spellInfo->Effect[i] == SPELL_EFFECT_SCRIPT_EFFECT || spellInfo->Effect[i] == SPELL_EFFECT_SEND_EVENT )
             {
                 found =  true;
                 break;
