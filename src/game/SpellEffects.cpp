0eef59c53f6226deabc7eb3895ab2f9f432efb8a
sixsixnine <none@mail.com>
2009-04-16 12:04:29 +0300

    + Nourish.
    Thanks to original creator(s).

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2494,6 +2494,19 @@ void Spell::EffectHeal( uint32 /*i*/ )
 
             addhealth += tickheal * tickcount;
         }
+        //Nourish 20% of heal increase if target is afected by Druids HOTs
+        else if(m_spellInfo->SpellFamilyFlags&0x0200000000000000LL){
+            Unit::AuraList const& RejorRegr = unitTarget->GetAurasByType(SPELL_AURA_PERIODIC_HEAL);
+
+            for(Unit::AuraList::const_iterator i = RejorRegr.begin(); i != RejorRegr.end(); ++i)
+            {
+                if((*i)->GetSpellProto()->SpellFamilyName == SPELLFAMILY_DRUID)
+                {
+                    addhealth+=addhealth*0.2;
+                    break;
+                }
+            }
+        }
         else
             addhealth = caster->SpellHealingBonus(unitTarget, m_spellInfo, addhealth, HEAL);
 
