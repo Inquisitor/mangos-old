9ea9204793949d51e20735a683d6d6ef3deaee8c
sixsixnine <none@mail.com>
2009-04-18 11:15:03 +0300

    + Seal of Wisdom

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5435,7 +5435,10 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 case 20186:
                 {
                     if (pVictim->getPowerType() == POWER_MANA)
-                        pVictim->CastSpell(pVictim, 20268, true, 0, triggeredByAura);
+                    {
+                        int32 bp = 2*(pVictim->GetCreateMana() / 100);
+                        pVictim->CastCustomSpell(pVictim, 20268, &bp, 0, 0, true, 0, triggeredByAura);
+                    }
                     return true;
                 }
                 // Holy Power (Redemption Armor set)
