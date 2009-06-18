16edd1ae171aa610aa0e9c9b31cdfb27fcc88e47
sixsixnine <none@mail.com>
2009-06-18 19:36:30 +0300

    + Lifebloom gaining mana back.
    Thanks to bobaz for this one.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2470,6 +2470,9 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                     {
                         int32 amount = m_modifier.m_amount/* / m_stackAmount*/;
                         m_target->CastCustomSpell(m_target, 33778, &amount, NULL, NULL, true, NULL, this, GetCasterGUID());
+
+                        int32 returnmana = (GetSpellProto()->ManaCostPercentage * caster->GetCreateMana() / 100) * m_stackAmount / 2;
+                        caster->CastCustomSpell(caster, 64372, &returnmana, NULL, NULL, true, NULL, this, GetCasterGUID());
                     }
                 }
                 return;
