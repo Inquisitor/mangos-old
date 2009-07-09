4263927d974d9f17029614667616f6a166fd11c6
Feanor <maly32167@wp.pl>
2009-07-09 13:39:13 +0200

    [Talent] Raptide

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2664,6 +2664,15 @@ void Spell::EffectHeal( uint32 /*i*/ )
                 }
             }
         }
+		// Chain Heal consumes Riptide
+        else if(m_spellInfo->SpellFamilyName == SPELLFAMILY_SHAMAN && m_spellInfo->SpellFamilyFlags == UI64LIT(0x100))
+        {
+            if (Aura *riptide = unitTarget->GetAura(SPELL_AURA_PERIODIC_HEAL, SPELLFAMILY_SHAMAN, 0, 0x10, m_caster->GetGUID()))
+            {
+                addhealth += addhealth * riptide->GetSpellProto()->EffectBasePoints[2] / 100;
+                unitTarget->RemoveAurasDueToSpell(riptide->GetId());
+            }
+        }
         else
             addhealth = caster->SpellHealingBonus(unitTarget, m_spellInfo, addhealth, HEAL);
 
