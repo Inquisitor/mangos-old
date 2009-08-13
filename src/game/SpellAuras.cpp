96ee42591cbe31deaebe42e46e8e0c0a788c5368
sixsixnine <none@mail.com>
2009-08-14 00:45:10 +0300

    + Vampiric Blood

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5133,11 +5133,15 @@ void Aura::HandleAuraModIncreaseHealth(bool apply, bool Real)
         case 55915:                                         // Battlemaster's trinket
         case 55917:                                         // Battlemaster's trinket
         case 50322:                                         // Survival Instincts
+        case 55233:                                         // Vampiric Blood
         {
             if(Real)
             {
                 if(apply)
                 {
+                     if (GetId() == 55233)
+                         m_modifier.m_amount *=  m_target->GetMaxHealth()/100;
+
                     m_target->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_VALUE, float(m_modifier.m_amount), apply);
                     m_target->ModifyHealth(m_modifier.m_amount);
                 }
