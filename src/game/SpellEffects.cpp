090cd759bd6a3a3d2f5fd54f4f95a2631fa3fb64
Feanor <maly32167@wp.pl>
2009-09-08 00:20:11 +0200

    new SummonType, fixes http://www.wowhead.com/?spell=43102 and http://www.wowhead.com/?spell=43042 summoning

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3640,6 +3640,7 @@ void Spell::EffectSummonType(uint32 i)
         break;
         case SUMMON_TYPE_WILD:
         case SUMMON_TYPE_WILD2:
+		case SUMMON_TYPE_WILD3:
             EffectSummonWild(i);
             break;
         case SUMMON_TYPE_DEMON:
