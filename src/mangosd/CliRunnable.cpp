739cbb4f665c72c23e673405017c1a5411e458a0
sixsixnine <none@mail.com>
2009-10-08 14:19:25 +0200

    + Implementing ChatHandler::HandleCharacterWhisperCommand() Allow whispering from RC to character and back using GM chat. GM level SEC_CONSOLE needed to use. syntax : character whisper toNick fromNick text ... NOTE: character can reply only to last GM spoke with him. NOTE2: character guid 1 is reserved.
    TODO : properly test.

--- src/mangosd/CliRunnable.cpp
+++ src/mangosd/CliRunnable.cpp
@@ -34,6 +34,8 @@
 #include "MapManager.h"
 #include "Player.h"
 #include "Chat.h"
+#include "WorldPacket.h"
+#include "../mangosd/RASocket.h"
 
 void utf8print(const char* str)
 {
@@ -105,6 +107,76 @@ bool ChatHandler::HandleAccountDeleteCommand(const char* args)
             return false;
     }
 
+    return true;
+}
+
+bool ChatHandler::HandleCharacterWhisperCommand(const char* args)
+{
+    if(!*args)
+        return false;
+
+    char *character_name_str = strtok((char*)args, " ");
+    if(!character_name_str)
+        return false;
+
+    std::string character_name = character_name_str;
+    if(!normalizePlayerName(character_name))
+        return false;
+
+    char *from_name_str = strtok(NULL, " ");
+    if(!from_name_str)
+        return false;
+
+    std::string from_name = from_name_str;
+    if(!normalizePlayerName(from_name))
+        return false;
+
+    Player *player = objmgr.GetPlayer(character_name.c_str());
+    if(!player || !player->GetSession())
+    {
+        char resultMsg[256];
+        snprintf( (char*)resultMsg, 256, "Character %s not found or not online\n", character_name.c_str());
+        RASocket::zprint(resultMsg);
+        return false;
+    }
+
+
+    from_name = std::string("[GM]") + from_name;
+    // set GM name for replies - more than one GM cannot speak with same player at same time
+    // maybe also needed in ::HandleNameQueryOpcode()  ?
+    // force SMSG_NAME_QUERY_RESPONSE if name changed to update client display
+    // note : guid 1 should be reserved dummy !!!
+    if (player->rcGmName != from_name)
+    {
+        player->rcGmName = from_name;
+
+        WorldPacket data( SMSG_NAME_QUERY_RESPONSE, (8+1+1+1+1+1+1+10) );
+        data.appendPackGUID(MAKE_NEW_GUID(1, 0, HIGHGUID_PLAYER));
+        data << uint8(0);             // added in 3.1
+        data << from_name.c_str();
+        data << uint32(0);            // not needed here data
+        data << uint8(0);             // is not declined
+        player->GetSession()->SendPacket(&data);
+    }
+
+    // all the tailing text is our msg
+    char* msgStr = strtok(NULL, "");
+    if(!msgStr)
+        return false;
+
+    std::string msg = msgStr;
+
+    WorldPacket data(SMSG_MESSAGECHAT, 200);
+    data << (uint8)CHAT_MSG_WHISPER;
+    data << (uint32)LANG_UNIVERSAL;
+    data << (uint64)1;              // from guid - reserved dummy
+    data << (uint32)LANG_UNIVERSAL;
+    data << (uint64)player->GetGUID();
+    data << (uint32)(strlen(msg.c_str())+1);
+    data << msg.c_str();
+    data << (uint8)4;
+
+    player->GetSession()->SendPacket(&data);
     return true;
 }
 
