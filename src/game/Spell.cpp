9af176306a2f0eb2481e7217c7add939b31d6753
Feanor <maly32167@wp.pl>
2009-12-06 14:55:21 +0100

    Warbringer - Charges will remove movement imparing effects

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4723,7 +4723,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             }
             case SPELL_EFFECT_CHARGE:
             {
-                if (m_caster->hasUnitState(UNIT_STAT_ROOT))
+                if (m_caster->hasUnitState(UNIT_STAT_ROOT) && !m_caster->HasAura(57499, 0))
                     return SPELL_FAILED_ROOTED;
 
                 break;
--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7336,6 +7336,17 @@ void Spell::EffectCharge(uint32 /*i*/)
     // not all charge effects used in negative spells
     if (unitTarget != m_caster && !IsPositiveSpell(m_spellInfo->Id))
         m_caster->Attack(unitTarget,true);
+
+    //Warbringer - remove movement imparing effects
+    Unit::AuraList const& auraClassScripts = m_caster->GetAurasByType(SPELL_AURA_OVERRIDE_CLASS_SCRIPTS);
+    for(Unit::AuraList::const_iterator itr = auraClassScripts.begin(); itr != auraClassScripts.end(); itr++)
+    {
+        if((*itr)->GetModifier()->m_miscvalue == 6953)
+        {
+            m_caster->RemoveAurasAtMechanicImmunity(IMMUNE_TO_ROOT_AND_SNARE_MASK,(*itr)->GetId(),true);
+            break;
+        }
+    }
 }
 
 void Spell::EffectCharge2(uint32 /*i*/)
