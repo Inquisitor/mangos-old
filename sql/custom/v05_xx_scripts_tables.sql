00c6ec2392c67820580097661bb2ac3b2ad9e9a2
Feanor <maly32167@wp.pl>
2009-12-09 18:21:05 +0100

    Added custom structure change for gossip_scripts

--- sql/custom/v05_xx_scripts_tables.sql
+++ sql/custom/v05_xx_scripts_tables.sql
@@ -17,3 +17,8 @@ ALTER TABLE gameobject_scripts
 ALTER TABLE event_scripts
   ADD COLUMN    requirement_type           int(11) unsigned NOT NULL default '0' AFTER delay,
   ADD COLUMN    requirement_value          int(11) unsigned NOT NULL default '0' AFTER requirement_type;
+
+
+ALTER TABLE gossip_scripts
+  ADD COLUMN    requirement_type           int(11) unsigned NOT NULL default '0' AFTER delay,
+  ADD COLUMN    requirement_value          int(11) unsigned NOT NULL default '0' AFTER requirement_type;
