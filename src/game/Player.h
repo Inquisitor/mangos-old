9ca3e483da4323273fd17da41eb5540fdb56ef73
Feanordev <feanordev@gmail.com>
2010-02-01 23:45:05 +0100

    + Player::HasQuest

--- src/game/Player.h
+++ src/game/Player.h
@@ -1443,6 +1443,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         void MoneyChanged( uint32 value );
         void ReputationChanged(FactionEntry const* factionEntry );
         bool HasQuestForItem( uint32 itemid ) const;
+        bool HasQuest( uint32 questId ) const;
         bool HasQuestForGO(int32 GOId) const;
         void UpdateForQuestWorldObjects();
         bool CanShareQuest(uint32 quest_id) const;
