0b5b586c49a6f753ec00f9ce40cc4f8d33ce717e
sixsixnine <none@mail.com>
2010-02-01 19:42:06 +0200

    + Safety checks in return values of SpellBaseDamageBonus / SpellBaseDamageBonusForVictim to prevent negative values -> overflows elsewhere.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9018,7 +9018,7 @@ int32 Unit::SpellBaseDamageBonus(SpellSchoolMask schoolMask)
         }
 
     }
-    return DoneAdvertisedBenefit;
+    return DoneAdvertisedBenefit > 0 ? DoneAdvertisedBenefit : 0;
 }
 
 int32 Unit::SpellBaseDamageBonusForVictim(SpellSchoolMask schoolMask, Unit *pVictim)
@@ -9042,7 +9042,7 @@ int32 Unit::SpellBaseDamageBonusForVictim(SpellSchoolMask schoolMask, Unit *pVic
             TakenAdvertisedBenefit += (*i)->GetModifier()->m_amount;
     }
 
-    return TakenAdvertisedBenefit;
+    return TakenAdvertisedBenefit > 0 ? TakenAdvertisedBenefit : 0;
 }
 
 bool Unit::isSpellCrit(Unit *pVictim, SpellEntry const *spellProto, SpellSchoolMask schoolMask, WeaponAttackType attackType)
