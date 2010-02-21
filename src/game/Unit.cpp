0ae1c28b704e1d98f51a58f0ec8697b2ac5aa786
sixsixnine <none@mail.com>
2010-02-21 17:24:17 +0200

    + Glyph of Rake.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6199,6 +6199,16 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     triggered_spell_id = 32747;
                     break;
                 }
+                // Glyph of Rake
+                case 54821:
+                {
+                    if (target->GetTypeId() == TYPEID_UNIT && procSpell->SpellVisual[0] == 750 && procSpell->EffectApplyAuraName[1] == 3)
+                    {
+                        triggered_spell_id = 54820;
+                        break;
+                    }
+                    return false;
+                }
                 // Glyph of Starfire
                 case 54845:
                 {
