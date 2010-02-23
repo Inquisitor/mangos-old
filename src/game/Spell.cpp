63ae434ede4edc2950e909eaa4dc6c8b9264f385
Feanordev <feanordev@gmail.com>
2010-02-23 21:17:47 +0100

    + Do not apply Cyclone when target is immune to spells (paladin shield etc)

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1056,6 +1056,12 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
 
     Unit* realCaster = GetAffectiveCaster();
 
+    if (m_spellInfo->Id == 33786 && unit->IsImmunedToDamage(GetSpellSchoolMask(m_spellInfo)))
+    {
+        realCaster->SendSpellMiss(unit, m_spellInfo->Id, SPELL_MISS_IMMUNE);
+        return;
+    }
+
     // Recheck immune (only for delayed spells)
     if( m_spellInfo->speed && unit->IsImmunedToSpell(m_spellInfo))
     {
