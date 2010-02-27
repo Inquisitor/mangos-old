5d30eeff71d17787c357c6d161b1af152c02eaac
Feanordev <feanordev@gmail.com>
2010-02-27 20:02:11 +0100

    + Some auras stacking problem solved

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1374,6 +1374,10 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
      if (spellInfo_1->Id == 26218 || spellInfo_2->Id == 26218)
          return false;
 
+     // Improved Mind Blast debuff stacks with everything
+     if (spellInfo_1->Id == 48301 || spellInfo_2->Id == 48301)
+         return false;
+
     // Specific spell family spells
     switch(spellInfo_1->SpellFamilyName)
     {
