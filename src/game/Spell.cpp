364376deedaf7d4b0977b3a8a9e63286063a3250
Feanordev <feanordev@gmail.com>
2010-03-15 21:59:27 +0100

    + Do not allow to use Ritual of Summoning in battleground

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4999,6 +4999,16 @@ SpellCastResult Spell::CheckCast(bool strict)
                     return SPELL_FAILED_BAD_TARGETS;
                 break;
             }
+            case SPELL_EFFECT_TRANS_DOOR:
+            {
+                if(m_caster->GetTypeId() == TYPEID_PLAYER)
+                {
+                    if(m_spellInfo->Id == 698)
+                        if(((Player*)m_caster)->GetMap()->IsBattleGround())
+                            return SPELL_FAILED_NOT_HERE;
+                }
+                break;
+            }
             default:break;
         }
     }
