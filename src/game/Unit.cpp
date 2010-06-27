4e6438a45f8ded6525518c5fe12d92e315cb72ff
Feanordev <feanordev@gmail.com>
2010-06-27 22:44:57 +0200

    + Players during Arena Preparation should not see opposing team

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11461,6 +11461,9 @@ bool Unit::isVisibleForOrDetect(Unit const* u, WorldObject const* viewPoint, boo
         invisible = false;
     }
 
+    if (HasAura(SPELL_ARENA_PREPARATION) && IsHostileTo(u))	
+        invisible = true;
+
     // special cases for always overwrite invisibility/stealth
     if(invisible || m_Visibility == VISIBILITY_GROUP_STEALTH)
     {
