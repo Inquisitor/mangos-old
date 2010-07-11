ccd43961917752944b3fbbf5c9a2c3546f269d22
Feanordev <feanordev@gmail.com>
2010-07-11 19:05:05 +0200

    + More generic stacking rule for Tsunami spells

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1902,9 +1902,8 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                         (spellInfo_2->Id == 39091 && spellInfo_1->Id == 39092))
                         return false;
 
-                    // Flame Tsunami Visual and Damage
-                    if ((spellInfo_1->Id == 57492 && spellInfo_2->Id == 57494) ||
-                        (spellInfo_2->Id == 57492 && spellInfo_1->Id == 57494))
+                    // All flame tsunami spells
+                    if(spellInfo_1->SpellIconID == 3087 && spellInfo_2->SpellIconID == 3087)
                         return false;
 
                     // Fury of Frostmourne
