824cdefcf28982dc398e6687972b4b1603ef8379
Feanordev <feanordev@gmail.com>
2010-08-11 14:54:42 +0200

    + Additional spell-target count limitations

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1581,6 +1581,8 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 case 29213:                                 // Curse of the Plaguebringer
                 case 31298:                                 // Sleep
                 case 60936:                                 // Surge of Power (25 man)
+                case 51904:                                 // Limiting the count of Summoned Ghouls
+                case 54522:
                     unMaxTargets = 3;
                     break;
                 case 30843:                                 // Enfeeble TODO: exclude top threat target from target selection
