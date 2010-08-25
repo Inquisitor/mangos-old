b33341bb98ca5d7425b2ee2bebe08a98f11ec6cd
sixsixnine <none@mail.com>
2010-08-25 20:08:03 +0300

    + Clean some junk leftovers.

--- sql/custom/v04_creature_ai_scripts.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE creature_ai_scripts
-  ADD COLUMN    event_requirement_type           tinyint(5) unsigned NOT NULL default '0' AFTER event_chance,
-  ADD COLUMN    event_requirement_value          int(11) unsigned NOT NULL default '0' AFTER event_requirement_type;
