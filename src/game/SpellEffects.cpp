5de2514ada08a60fcfa58f174aba4035990c53b3
Feanordev <feanordev@gmail.com>
2010-09-11 22:32:10 +0200

    fixed SMSG_RESYNC_RUNES struct and Empower Rune Weapon rune cooldown removing

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3201,6 +3201,19 @@ void Spell::EffectTriggerSpell(SpellEffectIndex effIndex)
             if (m_caster->HasAura(63093))
                 m_caster->CastSpell(m_caster, 65047, true); // Mirror Image
         }
+        // Empower Rune Weapon
+        case 53258:
+        {
+            // remove cooldown of all runes and send update to client
+            // +25 runic power already added in Spell::CheckOrTakeRunePower() in Spell::cast()
+            if(m_caster->GetTypeId() == TYPEID_PLAYER)
+            {
+                for(uint32 i = 0; i < MAX_RUNES; i++)
+                    ((Player*)m_caster)->SetRuneCooldown(i, 0);
+                ((Player*)m_caster)->ResyncRunes(MAX_RUNES);
+            }
+            return;
+        }
     }
 
     // normal case
