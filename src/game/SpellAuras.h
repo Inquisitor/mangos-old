4777bb73dcadc4bc44c879e473c683ab0db85bcd
Feanordev <feanordev@gmail.com>
2010-09-12 16:25:25 +0200

    + &Aura::HandleCharmConvert - to cleanup

--- src/game/SpellAuras.h
+++ src/game/SpellAuras.h
@@ -365,6 +365,7 @@ class MANGOS_DLL_SPEC Aura
         void HandleAuraOpenStable(bool apply, bool Real);
         void HandleAuraInitializeImages(bool Apply, bool Real);
         void HandleAuraCloneCaster(bool Apply, bool Real);
+        void HandleCharmConvert(bool apply, bool Real);
 
         virtual ~Aura();
 
