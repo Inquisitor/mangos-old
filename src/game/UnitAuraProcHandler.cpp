952290d4ed465b4fe0c812225b91b7aef75ff1a5
Warlord123 <Sergey@kwinta.net>
2010-10-30 22:38:20 +0600

    + Implement item 49982/50641 - Heartpierce
    -- (71880) Item - Icecrown 25 Normal Dagger Proc
    REPLACE INTO `spell_proc_event` VALUES (71880, 0x00, 0x0A, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x000000, 0x00000000, 1,0 , 0);
    -- (71892) Item - Icecrown 25 Heroic Dagger Proc
    REPLACE INTO `spell_proc_event` VALUES (71892, 0x00, 0x0A, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x000000, 0x00000000, 1, 0, 0);

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -2164,6 +2164,37 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
 
                     break;
                 }
+                // Item - Icecrown 25 Normal\Heroic Dagger Proc
+                case 71880://Normal
+                case 71892://Heroic
+                {
+                    if(GetTypeId() != TYPEID_PLAYER)
+                        return SPELL_AURA_PROC_FAILED;
+                    bool normal=(dummySpell->Id == 71880);
+                    switch(getPowerType())
+                    {
+                        case POWER_ENERGY:
+                        {
+                            triggered_spell_id= (normal ? 71882 : 71887);
+                            break;
+                        }
+                        case POWER_RAGE:
+                        {
+                            triggered_spell_id= (normal ? 71883 : 71886);
+                            break;
+                        }
+                        case POWER_MANA:
+                        {
+                            triggered_spell_id= (normal ? 71881 : 71888);
+                            break;
+                        }
+                        default:
+                            return SPELL_AURA_PROC_FAILED;
+                    }
+  
+                    target = this;
+                    break;
+                } 
             }
             break;
         }
