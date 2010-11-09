5d1b9727497b7f3d29d5484a1ac2fdf7aec717e7
unknown <Admin@.(none)>
2010-11-09 13:56:07 +0100

    + Healing Stream Totem healing bonus. (yay for new laptop and commit <3 (still no net though :S))

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2865,20 +2865,25 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
             {
                 if (unitTarget)
                 {
+                    int32 healval = m_caster->SpellDamageBonusDone(m_caster,m_spellInfo,damage,DOT);
+
                     if (Unit *owner = m_caster->GetOwner())
                     {
+                        if(owner->GetTypeId() == TYPEID_PLAYER) // Feanor: Must be hardcoded..?
+                            healval += ((Player*)owner)->GetBaseSpellPowerBonus() * 0.0827;
+
                         // Restorative Totems
                         Unit::AuraList const& mDummyAuras = owner->GetAurasByType(SPELL_AURA_DUMMY);
                         for(Unit::AuraList::const_iterator i = mDummyAuras.begin(); i != mDummyAuras.end(); ++i)
                             // only its have dummy with specific icon
                             if ((*i)->GetSpellProto()->SpellFamilyName == SPELLFAMILY_SHAMAN && (*i)->GetSpellProto()->SpellIconID == 338)
-                                damage += (*i)->GetModifier()->m_amount * damage / 100;
+                                healval += (*i)->GetModifier()->m_amount * damage / 100;
 
                         // Glyph of Healing Stream Totem
                         if (Aura *dummy = owner->GetDummyAura(55456))
-                            damage += dummy->GetModifier()->m_amount * damage / 100;
+                            healval += dummy->GetModifier()->m_amount * damage / 100;
                     }
-                    m_caster->CastCustomSpell(unitTarget, 52042, &damage, NULL, NULL, true, 0, 0, m_originalCasterGUID);
+                    m_caster->CastCustomSpell(unitTarget, 52042, &healval, NULL, NULL, true, 0, 0, m_originalCasterGUID);
                 }
                 return;
             }
