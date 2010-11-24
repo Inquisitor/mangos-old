4b2ee36539151de594b2993a47bf3b62d2edd919
Warlord123 <Sergey@kwinta.net>
2010-11-24 14:28:21 +0500

    + Fix hunter trap proc

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -768,8 +768,8 @@ void Spell::prepareDataForTriggerSystem()
                 break;
             case SPELLFAMILY_HUNTER:
                 // Hunter Rapid Killing/Explosive Trap Effect/Immolation Trap Effect/Frost Trap Aura/Snake Trap Effect/Explosive Shot
-                if ((m_spellInfo->SpellFamilyFlags & UI64LIT(0x0100200000000214)) ||
-                    m_spellInfo->SpellFamilyFlags2 & 0x40200)
+                if ((m_spellInfo->SpellFamilyFlags & UI64LIT(0x010020000000021C)) ||
+                    m_spellInfo->SpellFamilyFlags2 & 0x64200)
                     m_canTrigger = true;
                 break;
             case SPELLFAMILY_PALADIN:
@@ -837,7 +837,7 @@ void Spell::prepareDataForTriggerSystem()
 
     // Hunter traps spells (for Entrapment trigger)
     // Gives your Immolation Trap, Frost Trap, Explosive Trap, and Snake Trap ....
-    if (m_spellInfo->SpellFamilyName == SPELLFAMILY_HUNTER && (m_spellInfo->SpellFamilyFlags & UI64LIT(0x000020000000001C) || m_spellInfo->SpellFamilyFlags2 & 0x40000))
+    if (m_spellInfo->SpellFamilyName == SPELLFAMILY_HUNTER && (m_spellInfo->SpellFamilyFlags & UI64LIT(0x000020000000001C) || m_spellInfo->SpellFamilyFlags2 & 0x64000))
         m_procAttacker |= PROC_FLAG_ON_TRAP_ACTIVATION;
 }
 
