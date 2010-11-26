1a3a126d8031134d797e0cc48eafb9024a005fed
Feanor <Feanordev@gmail.com>
2010-11-26 19:43:03 +0100

    Glyph of Felguard

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -2240,6 +2240,8 @@ void Pet::ApplyAttackPowerScalingBonus(bool apply)
                 case CLASS_WARLOCK:
                 {
                     newAPBonus = std::max(owner->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_SHADOW),owner->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_FIRE));
+                    if(GetEntry() == 17252 && owner->HasAura(56246)) // Glyph of Felguard
+                        newAPBonus *= 1.2;
                     break;
                 }
                 case CLASS_DEATH_KNIGHT:
