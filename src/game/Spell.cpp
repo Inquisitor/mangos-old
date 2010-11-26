ea5aa2b37dde51f2b710f7ea7adaf58cecbbccff
Feanor <Feanordev@gmail.com>
2010-11-26 22:15:20 +0100

    Stoneclaw target hack

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2246,6 +2246,12 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             break;
         case TARGET_DUELVSPLAYER:
         {
+            if (m_spellInfo->SpellFamilyName == SPELLFAMILY_SHAMAN && m_spellInfo->SpellIconID == 276) // Stoneclaw Totem absorb has wrong target
+            {
+                targetUnitMap.push_back(m_caster);
+                break;
+            }
+
             Unit *target = m_targets.getUnitTarget();
             if(target)
             {
