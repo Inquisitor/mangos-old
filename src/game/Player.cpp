d804fe5821a4379cd117a426ec2862b0f55220bd
Feanordev <feanordev@gmail.com>
2010-12-22 02:26:53 +0100

    Glyph of Pick Pocket (hackish again but im too lazy for remaking it anyhow QQ)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7972,7 +7972,7 @@ void Player::SendLoot(ObjectGuid guid, LootType loot_type)
 
             // not check distance for GO in case owned GO (fishing bobber case, for example)
             // And permit out of range GO with no owner in case fishing hole
-            if (!go || (loot_type != LOOT_FISHINGHOLE && (loot_type != LOOT_FISHING || go->GetOwnerGuid() != GetObjectGuid()) && !go->IsWithinDistInMap(this,INTERACTION_DISTANCE)))
+            if (!go || (loot_type != LOOT_FISHINGHOLE && (loot_type != LOOT_FISHING || go->GetOwnerGuid() != GetObjectGuid()) && !go->IsWithinDistInMap(this,(loot_type == LOOT_PICKPOCKETING && HasAura(58017)) ? 10.0f : INTERACTION_DISTANCE)))
             {
                 SendLootRelease(guid);
                 return;
