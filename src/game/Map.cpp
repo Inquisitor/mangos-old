38210f26d0e3f9b11d10321fa26e3d1d5fac1901
Inquisitor <inquisitor@kwinta.net>
2010-12-25 23:01:55 +0200

    + Kick player out of instance instead of server crash

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1331,9 +1331,9 @@ bool InstanceMap::Add(Player *player)
                     GetInstanceSave()->GetMapId(), GetInstanceSave()->GetInstanceId(),
                     GetInstanceSave()->GetDifficulty(), GetInstanceSave()->GetPlayerCount(),
                     GetInstanceSave()->GetGroupCount(), GetInstanceSave()->CanReset());
-                MANGOS_ASSERT(false);
-                //player->RepopAtGraveyard();
-                //return false;
+                //MANGOS_ASSERT(false);
+                player->RepopAtGraveyard();
+                return false;
             }
         }
         else
@@ -1386,7 +1386,9 @@ bool InstanceMap::Add(Player *player)
                             sLog.outError("GroupBind save players: %d, group count: %d", groupBind->save->GetPlayerCount(), groupBind->save->GetGroupCount());
                         else
                             sLog.outError("GroupBind save NULL");
-                        MANGOS_ASSERT(false);
+                        //MANGOS_ASSERT(false);
+                        player->RepopAtGraveyard();
+                        return false;
                     }
                     // if the group/leader is permanently bound to the instance
                     // players also become permanently bound when they enter
@@ -1406,7 +1408,12 @@ bool InstanceMap::Add(Player *player)
                     player->BindToInstance(GetInstanceSave(), false);
                 else
                     // cannot jump to a different instance without resetting it
-                    MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+                    //MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+                    if (playerBind->save != GetInstanceSave())
+                    {
+                        player->RepopAtGraveyard();
+                        return false;
+                    }
             }
         }
     }
