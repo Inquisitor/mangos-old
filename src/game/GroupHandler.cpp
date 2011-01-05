12ea7436d68016ddbf6a6851c361d61a01f4146d
Feanordev <Feanordev@gmail.com>
2011-01-05 19:08:01 +0100

    Reverted - looks like needs isinworld checks and usch :S

--- src/game/GroupHandler.cpp
+++ src/game/GroupHandler.cpp
@@ -809,7 +809,7 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode( WorldPacket &recv_data )
     ObjectGuid guid;
     recv_data >> guid;
 
-    Player *player = HashMapHolder<Player>::Find(guid); 
+    Player *player = sObjectMgr.GetPlayer(guid);
     if(!player)
     {
         WorldPacket data(SMSG_PARTY_MEMBER_STATS_FULL, 3+4+2);
