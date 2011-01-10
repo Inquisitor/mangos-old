7553703f35e6ab4c28986aa3c82b283b123a46ae
Warlord123 <Sergey@kwinta.net>
2011-01-10 23:37:24 +0500

    + Fix Sindragosa spells hack by rsa.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1696,6 +1696,12 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 case 70836:                                 // Bone Storm
                     radius = DEFAULT_VISIBILITY_INSTANCE;
                     break;
+                case 69845:                                 // Sindragosa Frost bomb (hack!)
+                case 71053:
+                case 71054:
+                case 71055:
+                    radius = 50;
+                    break;
                 case 72350:                                 // Fury of Frostmourne
                 case 72351:                                 // Fury of Frostmourne 
                     radius = 300;
@@ -7036,7 +7042,22 @@ bool Spell::CheckTarget( Unit* target, SpellEffectIndex eff )
         if(((Player*)target)->isGameMaster() && !IsPositiveSpell(m_spellInfo->Id))
             return false;
     }
-
+     // Check Sated & Exhaustion debuffs
+     if (((m_spellInfo->Id == 2825) && (target->HasAura(57724))) ||
+         ((m_spellInfo->Id == 32182) && (target->HasAura(57723))))
+         return false;
+ 
+     // Check vampiric bite
+     if (m_spellInfo->Id == 70946 && target->HasAura(70867))
+         return false;
+ 
+     // Sindragosa frost bomb hack
+     if ((m_spellInfo->Id == 69845
+         || m_spellInfo->Id == 71053
+         || m_spellInfo->Id == 71054
+         || m_spellInfo->Id == 71055)
+         && target->HasAura(70867))
+         return false;
     // Check targets for LOS visibility (except spells without range limitations )
     switch(m_spellInfo->Effect[eff])
     {
