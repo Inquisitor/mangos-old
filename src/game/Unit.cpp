9fa6d29ce41490f7c0d0f2204d5d434379057f92
Feanordev <Feanordev@gmail.com>
2011-01-12 14:21:19 +0100

    So to continue - remove stealth auras too

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11810,6 +11810,7 @@ void Unit::EnterVehicle(VehicleKit *vehicle, int8 seatId)
     InterruptNonMeleeSpells(false);
     RemoveSpellsCausingAura(SPELL_AURA_MOUNTED);
     RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT);
+    RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
 
     if (!vehicle->AddPassenger(this, seatId))
         return;
