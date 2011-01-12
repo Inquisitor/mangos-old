5ec727e0722d92baac545502a4096222cf6efc04
Feanordev <Feanordev@gmail.com>
2011-01-12 14:18:28 +0100

    Remove Shapeshift auras at vehicle enter

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11809,6 +11809,7 @@ void Unit::EnterVehicle(VehicleKit *vehicle, int8 seatId)
 
     InterruptNonMeleeSpells(false);
     RemoveSpellsCausingAura(SPELL_AURA_MOUNTED);
+    RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT);
 
     if (!vehicle->AddPassenger(this, seatId))
         return;
