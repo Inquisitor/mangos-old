7244a9c3171fc8f4ba08bfa2515462e13645a2ef
Warlord123 <Sergey@kwinta.net>
2011-01-15 03:34:36 +0500

    + Fix Glyph of Scourge Strike.
    -- (58642) Glyph of Scourge Strike
    DELETE FROM `spell_proc_event` WHERE `entry` IN (58642);
    INSERT INTO `spell_proc_event` VALUES (58642, 0x7F, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x08000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000010, 0x00000000, 0, 101, 0);

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -1103,6 +1103,12 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                     }
 
 
+                    //Glyph of Scourge Strike
+                case 58642:
+                    {
+                        triggered_spell_id = 69961;
+                        break;
+                    }
         }
 
         break;
