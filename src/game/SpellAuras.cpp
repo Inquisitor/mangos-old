888f2e38c21b78e877cba676b53523a92a3a8f42
Feanordev <Feanordev@gmail.com>
2011-01-17 08:46:41 +0100

    Improve to AURA_STATE_BLEEDING - If there are more than one bleeding auras - unapply of one will not remove aurastate thx to Reamer

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8949,7 +8949,21 @@ void SpellAuraHolder::_RemoveSpellAuraHolder()
 
         // Mechanic bleed aura state
         if(GetAllSpellMechanicMask(m_spellProto) & (1 << (MECHANIC_BLEED-1)))
-            m_target->ModifyAuraState(AURA_STATE_BLEEDING, false);
+        {
+            // Look for another auras with same mechanic
+            bool found = false;
+            Unit::AuraList const& mPerDmg = m_target->GetAurasByType(SPELL_AURA_PERIODIC_DAMAGE);
+            for(Unit::AuraList::const_iterator itr = mPerDmg.begin(); itr != mPerDmg.end(); ++itr)
+            {
+                if (GetAllSpellMechanicMask((*itr)->GetSpellProto()) & (1<<(MECHANIC_BLEED-1)))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                m_target->ModifyAuraState(AURA_STATE_BLEEDING, false);
+        }
 
         uint32 removeState = 0;
         uint64 removeFamilyFlag = m_spellProto->SpellFamilyFlags;
